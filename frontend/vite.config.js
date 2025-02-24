ca0fccc2628ec11768aa13588a0f21b8bbf55462
Khushbu696 <khushbu.patel.cg@gmail.com>
2025-02-24 22:44:49 +0530

    moved .env package.json and package-lock.json in outer folder

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     //get rid of CORS error
     proxy: {
-      "api/" : {
+      "api/": {
         target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
